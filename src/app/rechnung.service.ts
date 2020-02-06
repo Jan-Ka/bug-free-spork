f9e7999c3a7276a9897b1b9ef16a1c578190fa86
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-06 21:12:20 +0000

    load demo JSON
    Map from the tool generated schema to the expected

--- src/app/rechnung.service.ts
+++ src/app/rechnung.service.ts
@@ -3,15 +3,15 @@ import { BehaviorSubject, Observable } from 'rxjs';
 
 import { IRechnung } from '../../shared/IRechnung';
 
-const StaticData: IRechnung[] = [
-  {
-    'Rechnungs-UID': '123abc',
-    Rechnungsnummer: 'RECH-123',
-    Rechnungsempfänger: 'Christopher Eo, Firma GmbH, Gebäudestraße 1, 1234 Stadtort',
-    'Betrag Netto': 12,
-    Datum: new Date()
-  }
-];
+import demoData from '../../rechnung_demo_2.json';
+
+interface IJsonRechnung {
+  'Rechnungs-UID': string;
+  Rechnungsnummer: string;
+  Rechnungsempfänger: string;
+  'Betrag Netto': string;
+  Datum: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -25,10 +25,22 @@ export class RechnungService {
   getAllRechnung(): Observable<IRechnung[]> {
     // init the BehaviorSubject if service wasn't used before
     if (!this.hot) {
-      this.allRechnung.next(StaticData);
+      this.allRechnung.next(this.normalizeDemoJson(demoData));
       this.hot = true;
     }
 
     return this.allRechnung;
+  }
+
+  private normalizeDemoJson(rawJson: IJsonRechnung[]): IRechnung[] {
+    return rawJson.map((val: IJsonRechnung) => {
+      return {
+        'Rechnungs-UID': val['Rechnungs-UID'],
+        Rechnungsnummer: val.Rechnungsnummer,
+        Rechnungsempfänger: val.Rechnungsempfänger,
+        'Betrag Netto': 0,
+        Datum: new Date()
+      };
+    });
   }
 }
