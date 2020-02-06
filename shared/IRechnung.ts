7433bc515dd07057c18acacf0cf7e18aa3a8be5f
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-06 19:51:07 +0000

    Added explanations to IRechnung properties

--- shared/IRechnung.ts
+++ shared/IRechnung.ts
@@ -3,9 +3,28 @@
  * @see {@link https://github.com/Jan-Ka/bug-free-spork/issues/16 Add `Rechnung` shared interface}
  */
 export interface IRechnung {
+    /**
+     * Identifies an `Rechnung` Object
+     */
     'Rechnungs-UID': string;
+
+    /**
+     * Sequential number
+     */
     Rechnungsnummer: string;
+
+    /**
+     * Recipient of `Rechnung` Objects
+     */
     Rechnungsempfänger: string;
+
+    /**
+     * net price; Sum of all `Rechnungspositionen\Product Betrag Netto`
+     */
     'Betrag Netto': number;
+
+    /**
+     * invoicing date
+     */
     'Datum': Date;
 }
