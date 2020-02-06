0a145e0fa22aa66526cd1175e57c4cdb130d513b
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-06 19:42:08 +0000

    Add InCode Documentation to IRechnung

--- shared/IRechnung.ts
+++ shared/IRechnung.ts
@@ -1,7 +1,11 @@
+/**
+ * Shared DTO to transport Rechnung Objects
+ * @see {@link https://github.com/Jan-Ka/bug-free-spork/issues/16 Add `Rechnung` shared interface}
+ */
 export interface IRechnung {
     'Rechnungs-UID': string;
     Rechnungsnummer: string;
     Rechnungsempfänger: string;
     'Betrag Netto': number;
     'Datum': Date;
-};
+}
