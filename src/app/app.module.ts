910fc0a7585194223ebc1e652d81282bb422fa2c
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-07 14:58:26 +0000

    Added \'RechnungpositionDetailDialog\'
    Resolves #37

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { RechnungpositionDetailDialogComponent } from './rechnungposition-detail-dialog/rechnungposition-detail-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RechnungpositionDetailDialogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/rechnungposition-detail-dialog/rechnungposition-detail-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rechnungposition-detail-dialog',
+  templateUrl: './rechnungposition-detail-dialog.component.html',
+  styleUrls: ['./rechnungposition-detail-dialog.component.scss']
+})
+export class RechnungpositionDetailDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
