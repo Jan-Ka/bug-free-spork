a5774e8c720406f6ee63b4a4c43724ad9e38f910
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-10 11:33:02 +0000

    Make shared Interfaces available through module

---
+++ shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { IRechnungFilter } from './IRechnungFilter';
+import { ILieferstatus } from './ILieferstatus';
+import { IRechnung } from './IRechnung';
+import { IRechnungsposition } from './IRechnungsposition';
+
+export {
+    IRechnungFilter,
+    ILieferstatus,
+    IRechnung,
+    IRechnungsposition
+};
