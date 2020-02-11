6b96de732ee348e506445b5ba333d2661a5f78fc
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-11 23:31:27 +0000

    fixed `RechnungspositionService\reviveRechnungspositionDemoData` not updating target
    refactoring and benchmarking

--- src/app/business-logic/rechnungsposition.service.ts
+++ src/app/business-logic/rechnungsposition.service.ts
@@ -24,12 +24,11 @@ export class RechnungspositionService {
   private readonly rechnungspositionDemoData: IRechnungsposition[];
 
   constructor() {
+    this.rechnungspositionDemoData = [];
     if (environment.demoData !== null) {
       RechnungspositionService.reviveRechnungspositionDemoData(this.rechnungspositionDemoData, environment.demoData.rechnungsposition);
 
       RechnungspositionService.generateDemoDataIndices(this.indexMap, this.rechnungspositionDemoData);
-    } else {
-      this.rechnungspositionDemoData = [];
     }
   }
 
@@ -40,15 +39,26 @@ export class RechnungspositionService {
   private static reviveRechnungspositionDemoData(target: IRechnungsposition[], importedData?: IJsonRechnungsposition[]): void {
     // missing file, empty file, broken JSON, wrong JSON are all be handled by the import
     // anything that makes it to here will crash in the frontend which is acceptable for this project
-    target = importedData.map((val: IJsonRechnungsposition) => {
-      return {
-        'Rechnungs-UID': val['Rechnungs-UID'],
-        'Produkt Name': val['Produkt Name'],
-        'Produkt Betrag Netto': parseFloat(val['Produkt Betrag Netto'])
-      };
-    });
+    // --
+    // until we get something like Array.addRange we have to apply some workaround
+    // to update target instead of overwriting it
+    // benchmarked this, 0.331ms|0.372ms|0.334ms for 1350 items
+    target.splice.apply(target, [
+      0,
+      1,
+      ...importedData.map((val: IJsonRechnungsposition) => {
+        return {
+          'Rechnungs-UID': val['Rechnungs-UID'],
+          'Produkt Name': val['Produkt Name'],
+          'Produkt Betrag Netto': parseFloat(val['Produkt Betrag Netto'])
+        };
+      })
+    ]);
   }
 
+  /**
+   * Generate a Map to find the indices of 'Rechnungs-UID' occurences in source
+   */
   private static generateDemoDataIndices(target: Map<string, number[]>, source: IRechnungsposition[]): void {
     source.forEach((rechnungsposition, index) => {
       const rechnungsUID = rechnungsposition['Rechnungs-UID'];
@@ -68,13 +78,14 @@ export class RechnungspositionService {
   private static getDemoDataByIndices(indices: number[], source: IRechnungsposition[]): IRechnungsposition[] {
     const result = [];
 
-    indices.sort((a, b) => a - b);
-
-    // maybe group by continuing indices and splice?
-    // filter by index?
-    for (const index of indices) {
-      result.push(source[index]);
-    }
+    // could probably be faster by grouping the indices for consecutive numbers and slicing
+    // for what we are doing currently, it's plenty fast
+    // benchmarked this, average of 0.01ms for 40 items
+    indices
+      .sort((a, b) => a - b)
+      .forEach((index) => {
+        result.push(source[index]);
+      });
 
     return result;
   }
