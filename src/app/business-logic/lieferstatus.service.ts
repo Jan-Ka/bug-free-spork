3acde8d4f7297ff9dacb43a4f7e02062fdc18cf9
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-11 00:06:10 +0000

    Add `LieferstatusService\getAllLieferstatus`
    Resolve #57

--- src/app/business-logic/lieferstatus.service.ts
+++ src/app/business-logic/lieferstatus.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { ILieferstatus } from 'shared/ILieferstatus';
+import { environment } from 'src/environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, take, withLatestFrom, finalize, toArray } from 'rxjs/operators';
+import { RechnungspositionService } from './rechnungsposition.service';
+import { IRechnungsposition } from 'shared/shared.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LieferstatusService {
+  private lookup: Map<string, string> = new Map();
+  private lieferstatusDemoData: ILieferstatus[];
 
-  constructor() { }
+  constructor(private rechnungspositionService: RechnungspositionService) {
+    if (environment.demoData !== null) {
+      this.lieferstatusDemoData = environment.demoData.lieferstatus;
+
+      this.lookup = LieferstatusService.generateDemoDataLookup(this.lieferstatusDemoData);
+    } else {
+      this.lieferstatusDemoData = null;
+    }
+  }
+
+  private static generateDemoDataLookup(source: ILieferstatus[]): Map<string, string> {
+    const result = new Map(source.map((val) => {
+      return [
+        val['Produkt Name'],
+        val.Lieferstatus
+      ];
+    }));
+
+    return result;
+  }
+
+  /**
+   * Retrieves Produkt Lieferstatus for a given Rechnungs-UID
+   * @param id Rechnungs-UID
+   */
+  getAllRechnungLieferstatus(id: string): Observable<ILieferstatus[]> {
+    return this.rechnungspositionService.getAllRechnungsposition(id).pipe(
+      map((value) => {
+        return {
+          'Produkt Name': value['Produkt Name'],
+          Lieferstatus: this.lookup.get(value['Produkt Name'])
+        } as ILieferstatus;
+      }),
+      toArray()
+    );
+  }
 }
