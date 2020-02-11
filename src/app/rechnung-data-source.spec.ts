030cbdf7e51e1d8bdc1817fe7f5fc7967b9db1fb
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-11 23:34:53 +0000

    removed unrelated files from wiki branch
    Resolves #65

--- src/app/rechnung-data-source.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RechnungDataSource } from './rechnung-data-source';
-
-describe('RechnungDataSource', () => {
-  it('should create an instance', () => {
-    expect(new RechnungDataSource(null)).toBeTruthy();
-  });
-});
