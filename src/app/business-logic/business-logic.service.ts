6a66ebe3a1109825cb7268e31a26ec0eb7e4051a
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-11 21:13:21 +0000

    moved retrieving of services into static class
    refactoring and documentation

--- src/app/business-logic/business-logic.service.ts
+++ src/app/business-logic/business-logic.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Injector } from '@angular/core';
+import { Injectable, Injector, Type, InjectionToken } from '@angular/core';
 import { RechnungService } from './rechnung.service';
 import { Observable } from 'rxjs';
 import { IRechnung, IRechnungsposition, ILieferstatus } from 'shared/shared.module';
@@ -16,43 +16,59 @@ export class BusinessLogicService {
 
   constructor(private injector: Injector) { }
 
+  /**
+   * Shortcut to load Service by token
+   * @param target singleton target
+   */
+  private static retrieveService<TService>(injector: Injector, token: Type<TService> | InjectionToken<TService>, target: TService) {
+    if (target === null) {
+      target = injector.get(token);
+    }
+
+    return target;
+  }
+
+  /**
+   * Retrieve a page of `IRechnung`
+   * @param pageIndex offset of first item
+   * @param pageSize max number of received items
+   */
   filterRechnung(pageIndex: number, pageSize: number): Observable<IRechnung[]> {
     return this.getRechnungService().filter(pageIndex, pageSize);
   }
 
+  /**
+   * Retrieve total count of available `IRechnung`
+   */
   availableRechnung(): Observable<number> {
     return this.getRechnungService().available();
   }
 
+  /**
+   * Receive all `IRechnungsposition` for provided `Rechnungs-UID`
+   * @param id `Rechnungs-UID`
+   */
   getAllRechnungsposition(id: string): Observable<IRechnungsposition[]> {
     return this.getRechnungspositionService().getAllRechnungsposition(id);
   }
 
+  /**
+   * Receive all `ILieferstatus` for provided `Rechnungs-UID`
+   * @param id 'Rechnungs-UID'
+   */
   getAllRechnungLieferstatus(id: string): Observable<ILieferstatus[]> {
     return this.getLieferstatusService().getAllRechnungLieferstatus(id);
   }
 
   private getRechnungService(): RechnungService {
-    if (this.rechnungService === null) {
-      this.rechnungService = this.injector.get(RechnungService);
-    }
-
-    return this.rechnungService;
+    return BusinessLogicService.retrieveService(this.injector, RechnungService, this.rechnungService);
   }
 
   private getRechnungspositionService(): RechnungspositionService {
-    if (this.rechnungspositionService === null) {
-      this.rechnungspositionService = this.injector.get(RechnungspositionService);
-    }
-
-    return this.rechnungspositionService;
+    return BusinessLogicService.retrieveService(this.injector, RechnungspositionService, this.rechnungspositionService);
   }
 
   private getLieferstatusService(): LieferstatusService {
-    if (this.lieferstatusService === null) {
-      this.lieferstatusService = this.injector.get(LieferstatusService);
-    }
-
-    return this.lieferstatusService;
+    return BusinessLogicService.retrieveService(this.injector, LieferstatusService, this.lieferstatusService);
   }
 }
