21952563f19347cb9327930e8ebdea2c99fe7f3a
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-12 23:06:03 +0000

    Display Errors in `RechnungTable`

--- src/app/rechnung-data-source.ts
+++ src/app/rechnung-data-source.ts
@@ -1,10 +1,12 @@
 import { IRechnung } from 'shared/IRechnung';
 import { DataSource } from '@angular/cdk/table';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, throwError, of } from 'rxjs';
 import { BusinessLogicService } from './business-logic/business-logic.service';
+import { catchError } from 'rxjs/operators';
 
 export class RechnungDataSource implements DataSource<IRechnung> {
     private rechnungSubject: BehaviorSubject<IRechnung[]> = new BehaviorSubject([]);
+    private errorSubject: BehaviorSubject<string> = new BehaviorSubject(null);
 
     constructor(private businessLogicService: BusinessLogicService) { }
 
@@ -20,8 +22,17 @@ export class RechnungDataSource implements DataSource<IRechnung> {
         return this.businessLogicService.availableRechnung();
     }
 
+    getError(): Observable<string> {
+        return this.errorSubject.asObservable();
+    }
+
     filter(pageIndex: number, pageSize: number): void {
-        this.businessLogicService.filterRechnung(pageIndex, pageSize).subscribe((value) => {
+        this.businessLogicService.filterRechnung(pageIndex, pageSize).pipe(
+            catchError((error) => {
+                this.errorSubject.next(error);
+                return of([]);
+            })
+        ).subscribe((value) => {
             this.rechnungSubject.next(value);
         });
     }
