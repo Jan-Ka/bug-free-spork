3df6080f898d6f2ab6b0ede3cdb486a6bf5f6b10
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-13 00:10:42 +0000

    add `returns empty array on no result` test for `RechnungspositionService`

--- src/app/business-logic/rechnungsposition/rechnungsposition.service.spec.ts
+++ src/app/business-logic/rechnungsposition/rechnungsposition.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RechnungspositionService } from './rechnungsposition.service';
+import { environment } from 'src/environments/environment';
 
 describe('RechnungspositionService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -8,5 +9,16 @@ describe('RechnungspositionService', () => {
   it('is created', () => {
     const service: RechnungspositionService = TestBed.get(RechnungspositionService);
     expect(service).toBeTruthy();
+  });
+
+  describe('getAllRechnungsposition', () => {
+    it('returns empty array on no result', (done) => {
+      environment.demoData = null;
+      const service: RechnungspositionService = TestBed.get(RechnungspositionService);
+      service.getAllRechnungsposition('').subscribe((value) => {
+        expect(value).toBe([]);
+        done();
+      });
+    });
   });
 });
