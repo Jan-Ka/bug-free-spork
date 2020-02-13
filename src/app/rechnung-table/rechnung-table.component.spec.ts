60d13a0f94edc9e544a939d5c4658197a1dfd1a0
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-13 00:31:24 +0000

    added missing declarations/imports for  test `RechnungTableComponent\is created`

--- src/app/rechnung-table/rechnung-table.component.spec.ts
+++ src/app/rechnung-table/rechnung-table.component.spec.ts
@@ -1,5 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BusinessLogicModule } from '../business-logic/business-logic.module';
+import { ErrorMessageComponent } from '../error-message/error-message/error-message.component';
+import { GetDetailButtonTooltipPipe } from './getDetailButtonTooltipPipe/get-detail-button-tooltip.pipe';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RechnungBetragNettoPipe } from '../rechnung-betrag-netto.pipe';
 import { RechnungTableComponent } from './rechnung-table.component';
 
 describe('RechnungTableComponent', () => {
@@ -8,9 +18,24 @@ describe('RechnungTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RechnungTableComponent ]
+      declarations: [
+        ErrorMessageComponent,
+        GetDetailButtonTooltipPipe,
+        RechnungBetragNettoPipe,
+        RechnungTableComponent
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        BusinessLogicModule,
+        MatButtonModule,
+        MatDialogModule,
+        MatIconModule,
+        MatPaginatorModule,
+        MatTableModule,
+        MatTooltipModule,
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +44,7 @@ describe('RechnungTableComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('creates', () => {
     expect(component).toBeTruthy();
   });
 });
