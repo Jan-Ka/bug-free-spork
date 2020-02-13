b0aa5a6dc3c6b22bbdb17ff06307534989a90a06
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-13 01:02:30 +0000

    add missing imports for test `RechnungspositionDetailService\creates it`

--- src/app/rechnungsposition-detail-table/rechnungsposition-detail-service/rechnungsposition-detail.service.spec.ts
+++ src/app/rechnungsposition-detail-table/rechnungsposition-detail-service/rechnungsposition-detail.service.spec.ts
@@ -1,11 +1,15 @@
+import { BusinessLogicModule } from 'src/app/business-logic/business-logic.module';
+import { RechnungspositionDetailService } from './rechnungsposition-detail.service';
 import { TestBed } from '@angular/core/testing';
 
-import { RechnungspositionDetailService } from './rechnungsposition-detail.service';
-
 describe('RechnungspositionDetailService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      BusinessLogicModule
+    ]
+  }));
 
-  it('should be created', () => {
+  it('creates it', () => {
     const service: RechnungspositionDetailService = TestBed.get(RechnungspositionDetailService);
     expect(service).toBeTruthy();
   });
