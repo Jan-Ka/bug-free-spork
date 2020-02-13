88e25bb49e132ed1b04b1fd9fd833d7d18b27b79
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-13 03:00:37 +0000

    added test to verify client requirement `Methode 2`

--- src/app/business-logic/business-logic.service.spec.ts
+++ src/app/business-logic/business-logic.service.spec.ts
@@ -2,9 +2,10 @@ import { BusinessLogicModule } from './business-logic.module';
 import { BusinessLogicService } from './business-logic.service';
 import { TestBed } from '@angular/core/testing';
 import { environment } from 'src/environments/environment';
-import { IRechnung } from 'shared/shared.module';
+import { IRechnung, ILieferstatus, IRechnungsposition } from 'shared/shared.module';
 import { RechnungService } from './rechnung/rechnung.service';
 import { of } from 'rxjs';
+import { RechnungspositionService } from './rechnungsposition/rechnungsposition.service';
 
 describe('BusinessLogicService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -46,6 +47,35 @@ describe('BusinessLogicService', () => {
       const service: BusinessLogicService = TestBed.get(BusinessLogicService);
 
       service.filterRechnung(0, 1).subscribe((value) => {
+        expect(value).toEqual([expected]);
+        done();
+      });
+    });
+
+    it('provides a method to receive \`Rechnungsposition\` for a given \`Rechnungs-UID\`', (done) => {
+      const expected: IRechnungsposition = {
+        'Rechnungs-UID': '123',
+        'Produkt Name': 'Abc',
+        'Produkt Betrag Netto': 1337.000
+      };
+
+      const lieferpositionSpy = jasmine.createSpyObj('RechnungspositionService', ['getAllRechnungsposition']);
+      lieferpositionSpy.getAllRechnungsposition.and.returnValue(
+        of(
+          [expected]
+        )
+      );
+
+      TestBed.configureTestingModule({
+        providers: [
+          {
+            provide: RechnungspositionService, useValue: lieferpositionSpy
+          }
+        ]
+      });
+
+      const service: BusinessLogicService = TestBed.get(BusinessLogicService);
+      service.getAllRechnungsposition(expected['Rechnungs-UID']).subscribe((value) => {
         expect(value).toEqual([expected]);
         done();
       });
