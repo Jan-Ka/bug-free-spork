87e8e11fb37dad6ce9835cee5ac5ddd2729bac2b
Jan-Ka <jan-ka@users.noreply.github.com>
2020-02-13 00:38:43 +0000

    added missing declarations/imports/providers for test `RechnungspositionDetailDialogComponent\is created`

--- src/app/rechnungsposition-detail-dialog/rechnungsposition-detail-dialog.component.spec.ts
+++ src/app/rechnungsposition-detail-dialog/rechnungsposition-detail-dialog.component.spec.ts
@@ -1,16 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BusinessLogicModule } from '../business-logic/business-logic.module';
+import { ErrorMessageComponent } from '../error-message/error-message/error-message.component';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { RechnungBetragNettoPipe } from '../rechnung-betrag-netto.pipe';
 import { RechnungspositionDetailDialogComponent } from './rechnungsposition-detail-dialog.component';
+import { RechnungspositionDetailTableComponent } from '../rechnungsposition-detail-table/rechnungsposition-detail-table.component';
 
-describe('RechnungpositionDetailDialogComponent', () => {
+describe('RechnungspositionDetailDialogComponent', () => {
   let component: RechnungspositionDetailDialogComponent;
   let fixture: ComponentFixture<RechnungspositionDetailDialogComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RechnungspositionDetailDialogComponent ]
+      declarations: [
+        ErrorMessageComponent,
+        RechnungBetragNettoPipe,
+        RechnungspositionDetailDialogComponent,
+        RechnungspositionDetailTableComponent
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        BusinessLogicModule,
+        MatDialogModule,
+        MatTableModule
+      ],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {}
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +46,7 @@ describe('RechnungpositionDetailDialogComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('is created', () => {
     expect(component).toBeTruthy();
   });
 });
